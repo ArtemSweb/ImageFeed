5919ec27a7b7259f0f7ff020f72f4b0cbb632086
ArtemSWeb <solodovnikov-artem@inbox.ru>
2025-01-02 11:41:25 +0300

    Добавлены IBOutlet + IBAction

--- ImageFeed/ImagesList/PrifileViewController.swift
+++ ImageFeed/ImagesList/PrifileViewController.swift
@@ -8,5 +8,14 @@
 import UIKit
 
 final class ProfileViewController: UIViewController {
+//MARK: - UI элементы
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var nicknameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var logoutButton: UIButton!
     
+//MARK: - функции кнопок
+    @IBAction func logoutFromProfile(_ sender: Any) {
+    }
 }
