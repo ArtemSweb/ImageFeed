325b970072aa69ed9deb13946f546708de8170e9
ArtemSWeb <solodovnikov-artem@inbox.ru>
2025-01-05 22:46:06 +0300

    добавлен экран для большой картинки + viewController

---
+++ ImageFeed/ImagesList/SingleImageViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Артем Солодовников on 05.01.2025.
+//
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
