556229b59ad1c1e3324fa2e1ab03c324140e2fb7
ArtemSWeb <solodovnikov-artem@inbox.ru>
2025-01-07 22:49:16 +0300

    добавлена кнопка шары

--- ImageFeed/ImagesList/SingleImageView/SingleImageViewController.swift
+++ ImageFeed/ImagesList/SingleImageView/SingleImageViewController.swift
@@ -13,6 +13,8 @@ final class SingleImageViewController: UIViewController {
 
             imageView.image = image
             imageView.frame.size = image.size
+            
+            doImageSizeAsDispkay(image: image)
         }
     }
 
@@ -27,11 +29,37 @@ final class SingleImageViewController: UIViewController {
         guard let image else { return }
         imageView.image = image
         imageView.frame.size = image.size
+        
+        doImageSizeAsDispkay(image: image)
     }
+    
+
+    //MARK: - Вспомогательные методы
+    private func doImageSizeAsDispkay(image: UIImage){
+        let minZoomScale = scrollView.minimumZoomScale
+        let maxZoomScale = scrollView.maximumZoomScale
+        view.layoutIfNeeded()
+        let visibleRectSize = scrollView.bounds.size
+        let imageSize = image.size
+        let hScale = visibleRectSize.width / imageSize.width
+        let vScale = visibleRectSize.height / imageSize.height
+        let scale = min(maxZoomScale, max(minZoomScale, min(hScale, vScale)))
+        scrollView.setZoomScale(scale, animated: false)
+        scrollView.layoutIfNeeded()
+        let newContentSize = scrollView.contentSize
+        let x = (newContentSize.width - visibleRectSize.width) / 2
+        let y = (newContentSize.height - visibleRectSize.height) / 2
+        scrollView.setContentOffset(CGPoint(x: x, y: y), animated: false)
+    }
+    
     
     //MARK: - кнопки
     @IBAction private func didTapReturnButton(_ sender: Any) {
         dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func didTapShareButton(_ sender: Any) {
+        
     }
 }
 
