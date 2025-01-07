721b152125265b16aa41a0067eb4a4c3fb3fb4db
ArtemSWeb <solodovnikov-artem@inbox.ru>
2025-01-07 16:17:37 +0300

    Реализован zoom

--- ImageFeed/ImagesList/SingleImageView/SingleImageViewController.swift
+++ ImageFeed/ImagesList/SingleImageView/SingleImageViewController.swift
@@ -9,21 +9,35 @@ import UIKit
 final class SingleImageViewController: UIViewController {
     var image: UIImage? {
         didSet {
-            guard isViewLoaded else { return }
+            guard isViewLoaded, let image else { return }
+
             imageView.image = image
+            imageView.frame.size = image.size
         }
     }
 
+    @IBOutlet private var scrollView: UIScrollView!
     @IBOutlet private var imageView: UIImageView!
-    @IBOutlet private weak var returnButton: UIButton!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        scrollView.minimumZoomScale = 0.1
+        scrollView.maximumZoomScale = 1.25
+
+        guard let image else { return }
         imageView.image = image
+        imageView.frame.size = image.size
     }
     
     //MARK: - кнопки
     @IBAction private func didTapReturnButton(_ sender: Any) {
         dismiss(animated: true, completion: nil)
+    }
+}
+
+//MARK: - расшрения
+extension SingleImageViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        imageView
     }
 }
