e835ec3f18ad500827fad93610dd0c4c52e7ae38
ArtemSWeb <solodovnikov-artem@inbox.ru>
2025-01-11 22:43:04 +0300

    верстка кодом ProfileViewController

--- ImageFeed/ImagesList/ProfileView/ProfileViewController.swift
+++ ImageFeed/ImagesList/ProfileView/ProfileViewController.swift
@@ -8,14 +8,71 @@
 import UIKit
 
 final class ProfileViewController: UIViewController {
-//MARK: - UI элементы
-    @IBOutlet private weak var nameLabel: UILabel!
-    @IBOutlet private weak var nicknameLabel: UILabel!
-    @IBOutlet private weak var descriptionLabel: UILabel!
-    @IBOutlet private weak var avatarImageView: UIImageView!
+    //MARK: - UI элементы + Верстка
     @IBOutlet private weak var logoutButton: UIButton!
     
-//MARK: - функции кнопок
-    @IBAction func logoutFromProfile(_ sender: Any) {
+    private let avatarImageView: UIImageView = {
+        let image = UIImage(named: "avatar")
+        let avatarImageView = UIImageView(image: image)
+        return avatarImageView
+    }()
+    
+    private let nameLabel: UILabel = {
+        let nameLabel = UILabel()
+        nameLabel.text = "Екатерина Новикова"
+        nameLabel.textColor = .ypWhite
+        nameLabel.font = UIFont.systemFont(ofSize: 23, weight: .bold)
+        return nameLabel
+    }()
+    
+    private let nicknameLabel: UILabel = {
+        let nicknameLabel = UILabel()
+        nicknameLabel.text = "@ekaterina_nov"
+        nicknameLabel.textColor = .ypGrey
+        nicknameLabel.font = UIFont.systemFont(ofSize: 13, weight: .regular)
+        return nicknameLabel
+    }()
+    
+    private let descriptionLabel: UILabel = {
+        let descriptionLabel = UILabel()
+        descriptionLabel.text = "Hello, world!"
+        descriptionLabel.textColor = .ypWhite
+        descriptionLabel.font = UIFont.systemFont(ofSize: 13, weight: .regular)
+        return descriptionLabel
+    }()
+    //стек для лейблов профиля
+    private let descriptionStackView: UIStackView = {
+        let descriptionStackView = UIStackView()
+        descriptionStackView.axis = .vertical
+        descriptionStackView.spacing = 8
+        return descriptionStackView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        addViews()
+        configurateConstraints()
+    }
+    
+    //MARK: - позиционирование элементов
+    private func addViews(){
+        [nameLabel, nicknameLabel, descriptionLabel].forEach { descriptionStackView.addArrangedSubview($0) }
+        [avatarImageView, descriptionStackView].forEach {
+            view.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+    }
+    
+    private func configurateConstraints(){
+        NSLayoutConstraint.activate([
+            avatarImageView.heightAnchor.constraint(equalToConstant: 70),
+            avatarImageView.widthAnchor.constraint(equalToConstant: 70),
+            avatarImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
+            avatarImageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            
+            descriptionStackView.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 8),
+            descriptionStackView.leadingAnchor.constraint(equalTo: avatarImageView.leadingAnchor)
+        ])
     }
 }
