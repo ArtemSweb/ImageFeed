8f73b796360cab1988c8d8f9e5025ffc61ac61a4
ArtemSWeb <solodovnikov-artem@inbox.ru>
2025-01-25 16:33:56 +0300

    add: добавлен экран входа

---
+++ ImageFeed/ImagesList/AuthView/AuthViewController.swift
@@ -0,0 +1,11 @@
+//
+//  AuthViewController.swift
+//  ImageFeed
+//
+//  Created by Артем Солодовников on 25.01.2025.
+//
+import UIKit
+
+final class AuthViewController: UIViewController {
+    
+}
--- ImageFeed/ImagesList/Constants.swift
+++ ImageFeed/ImagesList/Constants.swift
@@ -4,7 +4,12 @@
 //
 //  Created by Артем Солодовников on 19.01.2025.
 //
+import UIKit
 
-struct Constants {
-    
+enum Constants {
+    static let accessKey: String = "-dAHq51u5Is9hY_HYNx6VOF1hCubdOyy6eOtK4TEQbk"
+    static let secretKey: String = "wARpB7j0qw0OrAIZ-a9iScf1pcLLmt1e6qAAwyu3ozc"
+    static let redirectUri: String = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope: String = "public+read_user+write_likes"
+    static let defaultBaseIRL: URL = URL(string: "https://api.unsplash.com")!
 }
