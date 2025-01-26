f39ed2d9aa86f21c529e5e76dcd6a1b9e2c811e6
ArtemSWeb <solodovnikov-artem@inbox.ru>
2025-01-26 16:15:46 +0300

    add: реализовал получение токена(webView)

--- ImageFeed/ImagesList/AuthView/AuthViewController.swift
+++ ImageFeed/ImagesList/AuthView/AuthViewController.swift
@@ -7,5 +7,40 @@
 import UIKit
 
 final class AuthViewController: UIViewController {
+    private let buttonSegueIdentifier = "ShowWebView"
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureBackButton()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == buttonSegueIdentifier {
+            guard let webViewViewController = segue.destination as? WebViewViewController
+            else {
+                assertionFailure("Failed to prepare for \(buttonSegueIdentifier)")
+                return
+            }
+            webViewViewController.delegate = self
+        } else {
+            super.prepare(for: segue, sender: sender)
+        }
+    }
+    
+    //MARK: - стили
+    private func configureBackButton() {
+        navigationController?.navigationBar.backIndicatorImage = UIImage(named: "nav_back_button") // 1
+        navigationController?.navigationBar.backIndicatorTransitionMaskImage = UIImage(named: "nav_back_button") // 2
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil) // 3
+        navigationItem.backBarButtonItem?.tintColor = UIColor(named: "YP black") // 4
+    }
+}
+
+extension AuthViewController: WebViewViewControllerDelegate {
+    func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String) {
+    }
+    
+    func webViewViewControllerDidCancel(_ vc: WebViewViewController) {
+        vc.dismiss(animated: true)
+    }
 }
---
+++ ImageFeed/ImagesList/AuthView/WebViewViewController.swift
@@ -0,0 +1,77 @@
+//
+//  WebViewViewController.swift
+//  ImageFeed
+//
+//  Created by Артем Солодовников on 25.01.2025.
+//
+import UIKit
+import WebKit
+
+enum WebViewConstants {
+    static let unsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
+}
+
+protocol WebViewViewControllerDelegate: AnyObject {
+    func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String)
+    func webViewViewControllerDidCancel(_ vc: WebViewViewController)
+}
+
+final class WebViewViewController: UIViewController {
+    
+    @IBOutlet weak var webView: WKWebView!
+    
+    weak var delegate: WebViewViewControllerDelegate?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        webView.navigationDelegate = self
+        loadAuthView()
+    }
+    
+    //MARK: - Вспомогательные методы
+    private func loadAuthView(){
+        guard var urlComponents = URLComponents(string: WebViewConstants.unsplashAuthorizeURLString) else { return }
+        
+        urlComponents.queryItems = [
+            URLQueryItem(name: "client_id", value: Constants.accessKey),
+            URLQueryItem(name: "redirect_uri", value: Constants.redirectUri),
+            URLQueryItem(name: "response_type", value: "code"),
+            URLQueryItem(name: "scope", value: Constants.accessScope),
+        ]
+        
+        guard let url = urlComponents.url else { return }
+        let request = URLRequest(url: url)
+        webView.load(request)
+    }
+}
+
+//MARK: - Extensions
+extension WebViewViewController: WKNavigationDelegate {
+    func webView(
+        decidePolicyFor navigationAction: WKNavigationAction,
+        decisionHandler: @escaping (WKNavigationActionPolicy) -> Void
+    ) {
+        if let code = code(from: navigationAction) {
+            delegate?.webViewViewController(self, didAuthenticateWithCode: code)
+            decisionHandler(.cancel)
+        } else {
+            decisionHandler(.allow)
+        }
+    }
+    
+    private func code(from navigationAction: WKNavigationAction) -> String?{
+        if
+            let url = navigationAction.request.url,
+            let urlComponents = URLComponents(string: url.absoluteString),
+            urlComponents.path == "/oauth/authorize/native",
+            let items = urlComponents.queryItems,
+            let codeItem = items.first(where: { $0.name == "code" })
+        {
+            return codeItem.value
+        } else {
+            return nil
+        }
+    }
+}
+
